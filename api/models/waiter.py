57721bd496f7ec6c389513415fd62d0546261137
Luis Maciel <lmaciel.amador@gmail.com>
2025-02-17 23:35:29 -0600

    Modify string representation in Waiter Model

--- api/models/waiter.py
+++ api/models/waiter.py
@@ -12,3 +12,6 @@ class Waiter(User):
         """ Set is_staff for all waiters """
         self.is_staff = True
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
